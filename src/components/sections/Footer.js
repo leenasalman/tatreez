bc8d87a7a5711de2deb23a0aac1279d75fb32d5b
leenasalman <leenasalman.11@gmail.com>
2023-01-03 14:15:32 +0400

    footer

---
+++ src/components/sections/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Footer.css'
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div>
+          <h6 className="footer-style">©2023 Leena Salman. All rights reserved</h6>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
